930b530f228a16a3d5a122381c9d26a9a05adce8
James R. Qualls <Qualls.James@gmail.com>
2013-11-09 10:57:52 -0500

    removed papertrial logging.

--- config/logger.js
+++ config/logger.js
@@ -5,12 +5,12 @@ module.exports = {
     json: false,
     colorize: true
   },
-  papertrail: {
-    host: 'logs.papertrailapp.com',
-    port: 37571, // your port here
-    colorize: true,
-    hostname: "nexus-" + os.hostname()
-  },
+  // papertrail: {
+  //   host: 'logs.papertrailapp.com',
+  //   port: 37571, // your port here
+  //   colorize: true,
+  //   hostname: "nexus-" + os.hostname()
+  // },
   // file: {
   //   filename: 'app.log',
   //   // handleExceptions:true
